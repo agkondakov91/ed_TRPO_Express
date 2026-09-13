1cc39b4037ce4496b9ab0eae6f9403938f98fbd0
Alexander Kondakov <agkondakov91@gmail.com>
2026-09-13 19:10:24 +0300

    add biome, add simple routes

--- server.js
+++ server.js
@@ -3,8 +3,24 @@ import express from 'express';
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
+app.get('/', (_req, res) => {
     res.send('Привет! Это мой первый сервер на Express.');
+});
+
+app.get('/books', (_req, res) => {
+    res.status(200).send('Список книг');
+});
+
+app.post('/books', (_req, res) => {
+    res.status(201).send('Книга создана');
+});
+
+app.put('/books', (_req, res) => {
+    res.status(200).send('Книга обновлена');
+});
+
+app.delete('/books', (_req, res) => {
+    res.sendStatus(204);
 });
 
 app.listen(PORT, () => {
